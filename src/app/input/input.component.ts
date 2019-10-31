6cfdf913f04e475a757b1b21d435f6b1067b383a
Patrick <patrick@patrickvaneverdingen.design>
2019-10-31 21:11:09 +0100

    Change query URLs to correct values based on improved queries

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -13,14 +13,35 @@ export class InputComponent {
     selectedOption: string
 
     title = 'image-gallery'
-    private data: any = []
+    public data: any = []
+    public collectedData: any = []
+    public randomPortretData: any = []
+    public randomKledingData: any = []
 
     constructor(private http: HttpClient) {}
     generateData() {
         const queryURL = this.selectedOption
+        console.log(queryURL)
         this.http.get(queryURL).subscribe(res => {
-            // this.data = res
-            console.log(res)
+            this.data = res
+            this.collectedData.push(this.data) // Om een of andere reden moet this.data in een nieuwe array gestopt worden (collectedData)
+
+            this.collectedData[0].results.bindings.forEach(item => {
+                if (item.title.value.includes('vrouw') == true || item.title.value.includes(' man') == true) {
+                    this.randomPortretData.push(item)
+                }
+            })
+
+            this.collectedData[0].results.bindings.forEach(item => {
+                if (item.title.value.includes('kleding')) {
+                    this.randomKledingData.push(item)
+                }
+            })
+
+            var randomPortret = this.randomPortretData[Math.floor(Math.random() * this.randomPortretData.length)]
+            console.log('Random verzameling portretten:', randomPortret)
+            var randomKleding = this.randomKledingData[Math.floor(Math.random() * this.randomKledingData.length)]
+            console.log('Random kleding:', randomKleding)
         })
     }
 }
